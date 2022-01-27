55c69ac7f8fd2e3c879c8ec35c6a980b284525ed
Artem Kopytok <13ar.bass@gmail.com>
2022-01-27 18:49:43 +0300

    updated: final

--- main-page.js
+++ main-page.js
@@ -1,19 +1,49 @@
 let allClw = document.querySelectorAll('.clw__marq-row');
+let marqRow = document.querySelector('.marq-row');
 let currentMaqqPosition = 0;
-
+let liveMarq;
+let killMarq;
 const speed = 10;
 let currentSpeed = speed;
 
-let testInterval = setInterval(() => {
-    if (currentMaqqPosition <= 0 && currentMaqqPosition >= (-101)) {
-        currentMaqqPosition = currentMaqqPosition - .01;
+function startMarq() {
+    liveMarq = setInterval(() => {
+        if (currentMaqqPosition <= 0 && currentMaqqPosition >= (-101)) {
+            currentMaqqPosition = currentMaqqPosition - .01;
 
-        allClw.forEach(clw => {
-            clw.style.transform = ("translateX(" + currentMaqqPosition + "%)");
-        });
+            allClw.forEach(clw => {
+                clw.style.transform = ("translateX(" + currentMaqqPosition + "%)");
+            });
 
+        }
+        if (currentMaqqPosition <= (-100)) {
+            currentMaqqPosition = 0;
+        }
+    }, currentSpeed);
+}
+
+marqRow.addEventListener('mouseenter', function() {
+    killMarq = setInterval(() => {
+        currentSpeed = currentSpeed + 2;
+        clearTimeout(liveMarq);
+        startMarq();
+    }, 50);
+});
+
+setInterval(() => {
+    if (currentSpeed > 125) {
+        clearTimeout(liveMarq);
+        clearTimeout(killMarq);
     }
-    if (currentMaqqPosition <= (-100)) {
-        console.log('we can play again!');
-    }
-}, speed);
+}, 10);
+
+marqRow.addEventListener('mouseleave', function () {
+    clearTimeout(liveMarq);
+    clearTimeout(killMarq);
+    currentSpeed = speed;
+    startMarq();
+});
+
+
+
+startMarq();
