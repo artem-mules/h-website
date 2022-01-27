fb801e6c0d53d17feecdcd99a210900bcee18c66
Artem Kopytok <13ar.bass@gmail.com>
2022-01-27 17:45:36 +0300

    before: hover event

--- main-page.js
+++ main-page.js
@@ -1,1 +1,19 @@
-alert('hello marq! x) ');
+let allClw = document.querySelectorAll('.clw__marq-row');
+let currentMaqqPosition = 0;
+
+const speed = 10;
+let currentSpeed = speed;
+
+let testInterval = setInterval(() => {
+    if (currentMaqqPosition <= 0 && currentMaqqPosition >= (-101)) {
+        currentMaqqPosition = currentMaqqPosition - .01;
+
+        allClw.forEach(clw => {
+            clw.style.transform = ("translateX(" + currentMaqqPosition + "%)");
+        });
+
+    }
+    if (currentMaqqPosition <= (-100)) {
+        console.log('we can play again!');
+    }
+}, speed);
