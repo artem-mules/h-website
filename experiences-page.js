c90fc784cb7e6e2ba3fb5caace78d3aa43eefd4d
Artem Kopytok <13ar.bass@gmail.com>
2022-02-03 14:14:17 +0300

    updated: coma function

--- experiences-page.js
+++ experiences-page.js
@@ -1,3 +1,19 @@
+//create coma after catrgories names
+let allExpCards = document.querySelectorAll('.cl-i__filtered-content');
+
+allExpCards.forEach(expCard => {
+    let allCategoryNames = expCard.querySelectorAll('.experiences-card__meta-info-text--categories');
+    if (allCategoryNames.length > 0) {
+        allCategoryNames.forEach((categoryName, id) => {
+            if (id != (allCategoryNames.length - 1)) {
+                categoryName.textContent = (categoryName.textContent + ', ');
+            }
+        });
+    }
+});
+
+
+//make first filter's button active
 setTimeout(() => {
     let filterFirstButton = document.querySelector('.filter__rad-button');
     filterFirstButton.click();
