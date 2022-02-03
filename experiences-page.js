13ee15579fed74aabb6f9917f79151726f0633ee
Artem Kopytok <13ar.bass@gmail.com>
2022-02-03 15:29:49 +0300

    before: concatination script for category

--- experiences-page.js
+++ experiences-page.js
@@ -1,6 +1,18 @@
-//create coma after catrgories names
 let allExpCards = document.querySelectorAll('.cl-i__filtered-content');
+let expClw = document.querySelector('.clw__filtered-content');
 
+//create redirect with click on exp card
+expClw.addEventListener('click', function () {
+    allExpCards.forEach(expCard => {
+        let currentLink = expCard.querySelector('.hiiden-link');
+        expCard.addEventListener('click', function () {
+            window.location.href = currentLink;
+        });
+    });
+});
+
+
+//create coma after catrgories names
 allExpCards.forEach(expCard => {
     let allCategoryNames = expCard.querySelectorAll('.experiences-card__meta-info-text--categories');
     if (allCategoryNames.length > 0) {
