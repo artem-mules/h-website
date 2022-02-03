37423cea2ca6d7e70ab507bc62fecf767af9d61a
Artem Kopytok <13ar.bass@gmail.com>
2022-02-03 15:49:40 +0300

    updated

--- experiences-page.js
+++ experiences-page.js
@@ -1,28 +1,27 @@
 let allExpCards = document.querySelectorAll('.cl-i__filtered-content');
-let expClw = document.querySelector('.clw__filtered-content');
-
-//create redirect with click on exp card
-expClw.addEventListener('click', function () {
-    allExpCards.forEach(expCard => {
-        let currentLink = expCard.querySelector('.hiiden-link');
-        expCard.addEventListener('click', function () {
-            window.location.href = currentLink;
-        });
-    });
-});
 
 
 //create coma after catrgories names
-allExpCards.forEach(expCard => {
-    let allCategoryNames = expCard.querySelectorAll('.experiences-card__meta-info-text--categories');
-    if (allCategoryNames.length > 0) {
-        allCategoryNames.forEach((categoryName, id) => {
-            if (id != (allCategoryNames.length - 1)) {
-                categoryName.textContent = (categoryName.textContent + ', ');
+function checkCatNames() {
+    allExpCards.forEach(expCard => {
+        let allCurrentCatNames = expCard.querySelectorAll('.experiences-card__meta-info-text--categories-data');
+        let placeholderElement = expCard.querySelector('.experiences-card__meta-info-text--categories');
+        let placeholderData = '';
+        allCurrentCatNames.forEach((catName, id) => {
+            if (id != (allCurrentCatNames.length - 1)) {
+                placeholderData = (placeholderData + catName.textContent + ', ');
+            }
+            if (id == (allCurrentCatNames.length - 1)) {
+                placeholderData = (placeholderData + catName.textContent);
+                placeholderElement.textContent = placeholderData;
             }
         });
-    }
-});
+    });
+}
+
+setInterval(() => {
+    checkCatNames();
+}, 1000);
 
 
 //make first filter's button active
