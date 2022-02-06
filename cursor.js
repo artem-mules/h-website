29a73df3995cf3f5c48496dd71e9770b7451b248
Artem Kopytok <13ar.bass@gmail.com>
2022-02-06 22:22:31 +0300

    before: forks in function

--- cursor.js
+++ cursor.js
@@ -1,18 +1,37 @@
+// let cursorEl = document.querySelector('.cursor');
+// let currentElement = null;
+
+// document.addEventListener('mouseover', function (e) {
+//     currentElement = e.target;
+//     let attributesArray = new Array();
+//     attributesArray.push(currentElement.hasAttribute('data-cursor'));
+//     while (currentElement.parentElement != null) {
+//         currentElement = currentElement.parentElement;
+//         attributesArray.push(currentElement.hasAttribute('data-cursor'));
+//     }
+    
+//     if (attributesArray.includes(true)) {
+//         cursorEl.classList.add('cursor--active');
+//     } else {
+//         cursorEl.classList.remove('cursor--active');
+//     }
+// });
+
 let cursorEl = document.querySelector('.cursor');
 let currentElement = null;
 
 document.addEventListener('mouseover', function (e) {
     currentElement = e.target;
     let attributesArray = new Array();
-    attributesArray.push(currentElement.hasAttribute('data-cursor'));
+    attributesArray.push(currentElement);
     while (currentElement.parentElement != null) {
         currentElement = currentElement.parentElement;
-        attributesArray.push(currentElement.hasAttribute('data-cursor'));
+        attributesArray.push(currentElement);
     }
-    
-    if (attributesArray.includes(true)) {
-        cursorEl.classList.add('cursor--active');
-    } else {
-        cursorEl.classList.remove('cursor--active');
-    }
+
+    attributesArray.forEach(elHovered => {
+        if (elHovered.hasAttribute('data-cursor')) {
+            console.log(elHovered);
+        }
+    });
 });
