37a60499a14ea87708c4d5ee532c7b924aeba6f8
Artem Kopytok <13ar.bass@gmail.com>
2022-02-06 15:05:53 +0300

    updated: cursor script is works

--- cursor.js
+++ cursor.js
@@ -1,1 +1,13 @@
-console.log('hello CURSOR!');
+var currentElement = null;
+
+document.addEventListener('mouseover', function (e) {
+    currentElement = e.target;
+    let parentFind = currentElement.parentElement;
+    while (parentFind != null) {
+        if (parentFind.hasAttribute('data-cursor-text') || currentElement.hasAttribute('data-cursor-text')) {
+            console.log('это виджет, нужно изменить курсор');
+        }
+        parentFind = parentFind.parentElement;
+    } 
+    // console.log(parentFind);
+});
